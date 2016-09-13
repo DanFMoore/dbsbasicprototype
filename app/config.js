13beb02c967485d89877513ed7a7b2cc730406b6
Pete Craven <petecraven91@gmail.com>
2016-09-13 12:55:56 +0100

    Content updates

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Basic Disclosure',
+  serviceName: 'Basic DBS check',
 
   // Default port that prototype runs on
   port: '3000',
